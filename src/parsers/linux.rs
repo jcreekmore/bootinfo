ae83fa7942467c058ec65a9de2cbb1a47add7fb4
Jonathan Creekmore <jonathan@thecreekmores.org>
2018-01-24 10:53:35 -0600

    only print fields that are valid in the kernel version

--- src/parsers/linux.rs
+++ src/parsers/linux.rs
@@ -10,21 +10,32 @@ pub struct Header {
     header: u32,
     version_major: u8,
     version_minor: u8,
-    realmode_swtch: u32,
+    realmode_swtch: Option<u32>,
     kernel_version: Option<CString>,
-    load_flags: u8,
-    code32_start: u32,
-    initrd_addr_max: u32,
-    kernel_alignment: u32,
-    relocatable_kernel: bool,
-    min_alignment: u32,
-    xloadflags: u16,
-    cmdline_size: u32,
-    payload_offset: u32,
-    payload_length: u32,
+    load_flags: Option<u8>,
+    code32_start: Option<u32>,
+    initrd_addr_max: Option<u32>,
+    kernel_alignment: Option<u32>,
+    relocatable_kernel: Option<bool>,
+    min_alignment: Option<u32>,
+    xloadflags: Option<u16>,
+    cmdline_size: Option<u32>,
+    payload_offset: Option<u32>,
+    payload_length: Option<u32>,
     pref_address: Option<u64>,
-    init_size: u32,
-    handover_offset: u32,
+    init_size: Option<u32>,
+    handover_offset: Option<u32>,
+}
+
+fn valid<V>(version: u16, allowed: (u8, u8), value: V) -> Option<V> {
+    let major: u8 = (version >> 8) as u8;
+    let minor: u8 = (version & 0xff) as u8;
+
+    if major > allowed.0 || (major == allowed.0 && minor >= allowed.1) {
+        Some(value)
+    } else {
+        None
+    }
 }
 
 impl super::BootInfo for Header {}
@@ -65,8 +76,8 @@ impl Header {
         let major = version >> 8;
         let minor = version & 0xff;
 
-        // Explicitly not dealing with old boot protocols right now
-        if major < 2 || minor < 12 {
+        // Require versions at least 2.0 or newer
+        if major < 2 {
             return None;
         }
 
@@ -131,21 +142,21 @@ impl Header {
             header: header,
             version_major: major as u8,
             version_minor: minor as u8,
-            realmode_swtch: realmode_swtch,
+            realmode_swtch: valid(version, (2, 0), realmode_swtch),
             kernel_version: kernel_version,
-            load_flags: load_flags,
-            code32_start: code32_start,
-            initrd_addr_max: initrd_addr_max,
-            kernel_alignment: kernel_alignment,
-            relocatable_kernel: relocatable_kernel,
-            min_alignment: min_alignment,
-            xloadflags: xloadflags,
-            cmdline_size: cmdline_size,
-            payload_offset: payload_offset,
-            payload_length: payload_length,
-            pref_address: pref_address,
-            init_size: init_size,
-            handover_offset: handover_offset,
+            load_flags: valid(version, (2, 0), load_flags),
+            code32_start: valid(version, (2, 0), code32_start),
+            initrd_addr_max: valid(version, (2, 3), initrd_addr_max),
+            kernel_alignment: valid(version, (2, 5), kernel_alignment),
+            relocatable_kernel: valid(version, (2, 5), relocatable_kernel),
+            min_alignment: valid(version, (2, 10), min_alignment),
+            xloadflags: valid(version, (2, 12), xloadflags),
+            cmdline_size: valid(version, (2, 6), cmdline_size),
+            payload_offset: valid(version, (2, 8), payload_offset),
+            payload_length: valid(version, (2, 8), payload_length),
+            pref_address: pref_address.and_then(|addr| valid(version, (2, 10), addr)),
+            init_size: valid(version, (2, 10), init_size),
+            handover_offset: valid(version, (2, 11), handover_offset),
         };
 
         Some(Box::new(header) as Box<super::BootInfo>)
@@ -171,32 +182,50 @@ impl fmt::Display for Header {
                    self.setup_sects
                })?;
         write!(f, "  PM Code Size: {} bytes\n", (self.syssize * 16))?;
-        write!(f, "  Realmode Switch: 0x{:.08x}\n", self.realmode_swtch)?;
-        write!(f,
-               "  Loaded: {}\n",
-               if self.load_flags & 1 == 1 {
-                   "HIGH"
-               } else {
-                   "LOW"
-               })?;
-        write!(f, "  Code32 Start: 0x{:.08x}\n", self.code32_start)?;
-        write!(f, "  Initrd Addr Max: 0x{:.08x}\n", self.initrd_addr_max)?;
-        write!(f, "  Kernel Alignment: 0x{:.08x}\n", self.kernel_alignment)?;
-        write!(f,
-               "  Min. Kernel Alignment: 0x{:.08x}\n",
-               self.min_alignment)?;
-        write!(f, "  Relocatable?: {}\n", self.relocatable_kernel)?;
-        write!(f, "  xloadflags: 0x{:.04x}\n", self.xloadflags)?;
-        write!(f, "  Max Cmdline Size: {} bytes\n", self.cmdline_size)?;
-        write!(f, "  Payload Offset: 0x{:.08x}\n", self.payload_offset)?;
-        write!(f, "  Payload length: {} bytes\n", self.payload_length)?;
-        write!(f, "  init size: {} bytes\n", self.init_size)?;
+        if let Some(realmode_swtch) = self.realmode_swtch {
+            write!(f, "  Realmode Switch: 0x{:.08x}\n", realmode_swtch)?;
+        }
+        if let Some(load_flags) = self.load_flags {
+            write!(f,
+                   "  Loaded: {}\n",
+                   if load_flags & 1 == 1 { "HIGH" } else { "LOW" })?;
+        }
+        if let Some(code32_start) = self.code32_start {
+            write!(f, "  Code32 Start: 0x{:.08x}\n", code32_start)?;
+        }
+        if let Some(initrd_addr_max) = self.initrd_addr_max {
+            write!(f, "  Initrd Addr Max: 0x{:.08x}\n", initrd_addr_max)?;
+        }
+        if let Some(kernel_alignment) = self.kernel_alignment {
+            write!(f, "  Kernel Alignment: 0x{:.08x}\n", kernel_alignment)?;
+        }
+        if let Some(min_alignment) = self.min_alignment {
+            write!(f, "  Min. Kernel Alignment: 0x{:.08x}\n", min_alignment)?;
+        }
+        if let Some(relocatable_kernel) = self.relocatable_kernel {
+            write!(f, "  Relocatable?: {}\n", relocatable_kernel)?;
+        }
+        if let Some(xloadflags) = self.xloadflags {
+            write!(f, "  xloadflags: 0x{:.04x}\n", xloadflags)?;
+        }
+        if let Some(cmdline_size) = self.cmdline_size {
+            write!(f, "  Max Cmdline Size: {} bytes\n", cmdline_size)?;
+        }
+        if let Some(payload_offset) = self.payload_offset {
+            write!(f, "  Payload Offset: 0x{:.08x}\n", payload_offset)?;
+        }
+        if let Some(payload_length) = self.payload_length {
+            write!(f, "  Payload length: {} bytes\n", payload_length)?;
+        }
+        if let Some(init_size) = self.init_size {
+            write!(f, "  init size: {} bytes\n", init_size)?;
+        }
         if let Some(ref address) = self.pref_address {
             write!(f, "  Preferred load address: 0x{:.016x}\n", address)?;
         }
-        write!(f,
-               "  EFI Handover Offset: 0x{:.08x}\n",
-               self.handover_offset)?;
+        if let Some(handover_offset) = self.handover_offset {
+            write!(f, "  EFI Handover Offset: 0x{:.08x}\n", handover_offset)?;
+        }
         Ok(())
     }
 }
